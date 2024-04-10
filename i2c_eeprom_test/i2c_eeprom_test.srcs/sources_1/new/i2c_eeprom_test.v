4e23478dcaea666c9b65d96b19ab07ee13baef50
liuyong <xin_nian13@163.com>
2024-04-10 17:24:34 +0800

    从零开始写的按键消抖vivado验证可用

--- i2c_eeprom_test/i2c_eeprom_test.srcs/sources_1/new/i2c_eeprom_test.v
+++ i2c_eeprom_test/i2c_eeprom_test.srcs/sources_1/new/i2c_eeprom_test.v
@@ -21,54 +21,84 @@
 
 
 module i2c_eeprom_test(
-    input            sys_clk,       //system clock 50Mhz on board
-    input            rst_n,         //reset ,low active
-    input            key1,          //user key on board
-    inout            i2c_sda,       // i2c data
-    inout            i2c_scl,       // i2c clock
-    output [3:0]     led            //user leds on board
+    input                   sys_clk,        //system clock 50Mhz on board
+    input                   rst_n,          //reset ,low active
+    input                   key1,           //user key on board
+    inout                   i2c_sda,        // i2c data
+    inout                   i2c_scl,        // i2c clock
+    output reg [3:0]        led             //user leds on board
     );
-    
-//===========================================================================
-// PORT declarations
-//===========================================================================
 
-//define the time counter
-reg [31:0]   timer;                  
-reg [3:0]    led;
-
-          
-//===========================================================================
-// cycle counter:from 0 to 4 sec
-//===========================================================================
-  always @(posedge sys_clk or negedge rst_n)    
-    begin
-      if (~rst_n)                           
-          timer <= 32'd0;                     // when the reset signal valid,time counter clearing
-      else if (timer == 32'd199_999_999)    //4 seconds count(50M*4-1=199999999)
-          timer <= 32'd0;                       //count done,clearing the time counter
-      else
-		    timer <= timer + 1'b1;            //timer counter = timer counter + 1
-    end
+parameter				TWENTY_SECOND_CLOCKS = 1000000;
+reg						key1_1d;
+reg						key1_2d;
+wire					key1_xor;
+wire					key1_posedge;
+wire					key1_negedge;
+reg [31:0]				counter;
+reg [31:0]				counter_1d;
 
 //===========================================================================
-// LED control
+// 
 //===========================================================================
-  always @(posedge sys_clk or negedge rst_n)   
-    begin
-      if (~rst_n)                      
-          led <= 4'b0000;                  //when the reset signal active         
-      else if (timer == 32'd49_999_999)    //time counter count to 1st sec,LED1 lighten
-    
-          led <= 4'b0001;                 
-      else if (timer == 32'd99_999_999)    //time counter count to 2nd sec,LED2 lighten
-      begin
-          led <= 4'b0010;                  
-        end
-      else if (timer == 32'd149_999_999)   //time counter count to 3nd sec,LED3 lighten
-          led <= 4'b0100;                                          
-      else if (timer == 32'd199_999_999)   //time counter count to 4nd sec,LED4 lighten
-          led <= 4'b1000;                         
-    end
+assign key1_xor = key1_1d ^ key1_2d;
+assign key1_negedge = (counter == TWENTY_SECOND_CLOCKS - 1) & (counter_1d == TWENTY_SECOND_CLOCKS) & (key1_1d == 1'b0);
+assign key1_posedge = (counter == TWENTY_SECOND_CLOCKS - 1) & (counter_1d == TWENTY_SECOND_CLOCKS) & (key1_1d == 1'b1);
+
+always@(posedge key1_posedge or negedge rst_n)
+begin
+	if (rst_n == 1'b0)
+		led <= 4'b0000;
+	else
+		led <= ~(~led + 4'b1);
+end
+
+always@(posedge sys_clk or negedge rst_n)
+begin
+	if (rst_n == 1'b0)
+	begin
+		key1_1d <= 1'b1;
+		key1_2d <= 1'b1;
+	end
+	else
+	begin
+		key1_1d <= key1;
+		key1_2d <= key1_1d;
+	end
+end
+
+always@(key1_xor, counter)
+begin
+	if (key1_xor == 1'b1)
+		counter_1d <= 32'h00000000;
+	else if (counter < TWENTY_SECOND_CLOCKS)
+		counter_1d <= counter + 1'b1;
+	else
+		counter_1d <= counter;
+end
+
+always@(posedge sys_clk or negedge rst_n)
+begin
+	if (rst_n == 1'b0)
+	begin
+		counter <= 32'h00000000;
+	end
+	else
+	begin
+		counter <= counter_1d;
+	end
+end
+
+//===========================================================================
+// 
+//===========================================================================
+
+
+
+//===========================================================================
+// 
+//===========================================================================
+
+
 
 endmodule
